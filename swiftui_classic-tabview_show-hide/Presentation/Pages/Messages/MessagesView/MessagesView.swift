b08275a49d5fd9ecad816da41bb6f180dbf7d193
Xiang Fei <nexroxf@gmail.com>
2023-10-27 18:54:33 +0800

    first commit

--- swiftui_classic-tabview_show-hide/Presentation/Pages/Messages/MessagesView/MessagesView.swift
+++ swiftui_classic-tabview_show-hide/Presentation/Pages/Messages/MessagesView/MessagesView.swift
@@ -4,6 +4,7 @@ struct MessagesView: View {
     let props: Props
 
     var body: some View {
+        NavigationLink(destination: CameraView(), label: {Text("camera")})
         List {
             Section(header: owner) {
                 ForEach((1...props.fakeMsgsCount), id: \.self) {
@@ -26,6 +27,96 @@ struct MessagesView: View {
                 Text("message \(index) ")
                 Text("timestamp: \(Date().timeIntervalSince1970)")
             }
+        }
+    }
+}
+
+struct CameraView: View {
+    @State private var isCameraPresented: Bool = false
+    @State private var selectedImage: UIImage?
+
+    var body: some View {
+        VStack {
+            if let image = selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+
+            Button("打开相机") {
+                isCameraPresented = true
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+        }
+        .fullScreenCover(isPresented: $isCameraPresented) {
+            ImagePicker(sourceType: .camera, selectedImage: $selectedImage)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+//import SwiftUI
+import AVFoundation
+
+struct ImagePicker: UIViewControllerRepresentable {
+    var sourceType: UIImagePickerController.SourceType
+    @Binding var selectedImage: UIImage?
+    
+    // 根据设备能力决定是否使用 Portrait 模式
+    var usePortraitMode: Bool {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            if let cameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+                let supportedFormats = cameraDevice.formats
+                for format in supportedFormats {
+                    if !format.supportedDepthDataFormats.isEmpty {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        
+        // 如果设备支持 Portrait 模式，则启用
+        if usePortraitMode {
+            print("xxxxx")
+            picker.cameraCaptureMode = .photo
+            picker.cameraDevice = .rear
+        } else {
+            print("yyyyy")
+            // 如果不支持 Portrait 模式，你可以选择其他设置或者什么都不做
+        }
+        
+        return picker
+    }
+
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            picker.dismiss(animated: true)
         }
     }
 }
